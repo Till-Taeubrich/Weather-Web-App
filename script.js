9b8138d651075d40ff1a84c41684ec28280d38e8
Till-Taeubrich <Till.taeubrich@gmail.com>
2022-03-28 19:10:07 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchBar = document.querySelector('#search-bar');
+
+async function getWeatherData() {
+  const userSearch = searchBar.value;
+  try {
+    const response = await fetch(
+      `http://api.ofetpenweathermap.org/data/2.5/weather?q=${userSearch}&APPID=ec14f259ccf044b9dbeec27752c8e621`,
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    const temperature = weatherData.main.temp;
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function getTemperature() {}
+
+searchBar.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    getWeatherData();
+  }
+});
